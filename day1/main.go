cd7bfeccc2bd1dbfac8bc2cc32d2c63f9c994f15
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-12-10 17:21:21 +0000

    Day 1 solution

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type table struct {
+	leftCol  []int
+	rightCol []int
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	filePath := "input.txt"
+	input, err := readInput(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Sort both columns in ascending order
+	sort.Ints(input.leftCol)
+	sort.Ints(input.rightCol)
+
+	diff := 0
+	total := 0
+	for i, left := range input.leftCol {
+		diff = left - input.rightCol[i]
+		if diff < 0 {
+			diff = -diff
+		}
+		total += diff
+	}
+	fmt.Println("Result: ", total)
+}
+
+func readInput(filePath string) (*table, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	out := &table{}
+
+	for scanner.Scan() {
+		line := scanner.Text()          // Read the line as a string
+		columns := strings.Fields(line) // For space/tab-separated data
+
+		firstNumber, err := strconv.Atoi(columns[0])
+		if err != nil {
+			fmt.Printf("Error converting first number to integer: %v\n", err)
+			return nil, err
+		}
+
+		secondNumber, err := strconv.Atoi(columns[1])
+		if err != nil {
+			fmt.Printf("Error converting second number to integer: %v\n", err)
+			return nil, err
+		}
+
+		out.leftCol = append(out.leftCol, firstNumber)
+		out.rightCol = append(out.rightCol, secondNumber)
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return nil, err
+	}
+	return out, nil
 }
